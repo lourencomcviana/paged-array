af423b6df41c2f39a8d649e539125144e22f4786
lourenço viana <lourenco.m.c.viana@gmail.com>
2017-08-22 15:29:09 -0300

    unworking example at the moment, old 'main'

---
+++ examples/basic.js
@@ -0,0 +1,14 @@
+import  PagedArray from "./PagedArray";
+
+var arr=new PagedArray(["maria","123","maria","123","maria","123","maria","123"],{
+    size:2,
+    run:function(data,info){
+
+        return data+" - "+info.pageCurrent;
+    }
+});
+
+arr.forEach(function(item:any,id:any){
+    console.log(item);
+
+});
