278319dbe28cae71045fa9230b7549f34e8c1e57
lourenço viana <lourenco.m.c.viana@gmail.com>
2017-09-12 17:44:55 -0300

    PagedArray now works with clientPaging

--- src/PagedArray.ts
+++ src/PagedArray.ts
@@ -27,6 +27,7 @@ interface ProgressFunction{
 
 interface Options{
     [index: string]: any;
+    clientPaging?:boolean,
     size?:number;
     run:RunFunction;
     //Will replace with Progress later, converting to typescript
@@ -54,126 +55,173 @@ class Progress{
 }
 
 export default class PagedArray extends Array implements PageInfo {
+
+  private config :Config;
+  private parameterList :Array<any> ;
+  private parameterNumber:number;
+
+  constructor(parameter :Array<any>|number ,options:Options|RunFunction) {
+    super();
+    this.config=<Config>PagedArray.ProcessOptions(options);
+
+    this.config.current=0;
+    if(typeof parameter=='number'){
+      this.parameterNumber=<number>parameter; 
+    }else{
+      this.parameterList=<Array<any>>parameter;
+    }
+    
+
+  }
   
-    private config :Config;
-    private parameterList :Array<any> ;
-
-    constructor(parameterList :Array<any> ,options:Options|RunFunction) {
-      super();
-      this.config=<Config>PagedArray.ProcessOptions(options);
-
-      this.config.current=0;
-
-      this.parameterList=parameterList;
-
+  private genInfo():PageInfo{
+    return {
+      parameters:this.parameters,
+      pageSize:this.pageSize,
+      pageCurrent:this.pageCurrent,
+      pageTotal:this.pageTotal,
+      pageItemRange:this.pageItemRange,
+      pageStart:this.pageStart,
+      pageEnd:this.pageEnd
     }
+  }
+
+  static ProcessOptions(options:Options|RunFunction) :Options{
+    let model:Options={
+      size:100,
+      clientPaging:false,
+      run:undefined,
+      progress:undefined,
+    };
     
-    private genInfo():PageInfo{
-      return {
-        parameters:this.parameters,
-        pageSize:this.pageSize,
-        pageCurrent:this.pageCurrent,
-        pageTotal:this.pageTotal,
-        pageItemRange:this.pageItemRange,
-        pageStart:this.pageStart,
-        pageEnd:this.pageEnd
-      }
-    }
+    if(typeof options=='function'){
+      model.run=options;
+    } else if(typeof options=='object'){
+      options=<Options>options;
 
-    static ProcessOptions(options:Options|RunFunction) :Options{
-      let model:Options={
-        size:100,
-        run:undefined,
-        progress:undefined,
-      };
+      for(let id in model){
+        if(!options[id]) options[id]=model[id];
+      }
+
       
-      if(typeof options=='function'){
-        model.run=options;
-      } else if(typeof options=='object'){
-        options=<Options>options;
+      return options;
+    }
+    return model;
+  }
 
-        for(var id in model){
-          if(!options[id]) options[id]=model[id];
-        }
-        return options;
+  get parameters() {
+      return this.parameterList;
+  }
+
+  get pageSize() {
+      return this.config.size;
+  }
+
+  get pageCurrent() {
+      return this.config.current;
+  }
+
+  get pageTotal() {
+    if(this.parameterNumber)
+      return this.parameterNumber;
+    return Math.ceil(this.parameters.length/this.pageSize);
+  }
+  
+  get pageItemRange() {
+    let itemSize=this.pageSize*this.pageCurrent
+      return{
+        min:itemSize,
+        max:itemSize+this.pageSize
+      };
+  }
+
+  get pageStart() {
+    return this.pageSize*this.pageCurrent;
+
+  }
+
+  get pageEnd() {
+    let end=this.pageStart+this.pageSize;
+    let paramLength= this.parameterNumber?this.parameterNumber:this.parameters.length;
+    return end>paramLength?paramLength:end;
+  }
+
+  hasNext(){
+    return (this.pageCurrent<this.pageTotal-1)  
+  }
+
+  hasPrevious(){
+    return (this.pageCurrent>0)
+  }
+  
+  next(){
+    if(this.hasNext()){
+      this.config.current++;
+      this.load();
+      return true;
+    }
+    return false;
+  }
+
+
+  previous(){
+    if(this.hasPrevious()){
+      this.config.current--;
+      this.load();
+      return true;
+    }
+    return false;
+  }
+
+  load(progress?:Progress|ProgressFunction){
+    this.length = 0;
+    if(!progress || typeof(progress)=='function')
+      progress = new Progress(this.config.progress)
+    progress=<Progress>progress;
+
+    progress.start(this.pageEnd-this.pageStart);    
+
+  
+    //convert requests to match paging
+    if(this.config.clientPaging){
+      let result;
+      if(this.parameterNumber){
+        result=this.config.run(this.parameterNumber,this.genInfo())
+      }else{
+        result=this.config.run(this.parameterList[this.pageCurrent],this.genInfo())
       }
-      return model;
-    }
-
-    get parameters() {
-        return this.parameterList;
-    }
-
-    get pageSize() {
-        return this.config.size;
-    }
-
-    get pageCurrent() {
-        return this.config.current;
-    }
-
-    get pageTotal() {
-        return Math.ceil(this.parameters.length/this.pageSize);
-    }
-    
-    get pageItemRange() {
-      let itemSize=this.pageSize*this.pageCurrent
-        return{
-          min:itemSize,
-          max:itemSize+this.pageSize
-        };
-    }
-
-    get pageStart() {
-      return this.pageSize*this.pageCurrent;
-
-    }
-
-    get pageEnd() {
-      let end=this.pageStart+this.pageSize;
-      return end>this.parameters.length?this.parameters.length:end;
-    }
-
-    hasNext(){
-      return (this.pageCurrent<this.pageTotal-1)
-    }
-
-    hasPrevious(){
-      return (this.pageCurrent>0)
-    }
-    
-    next(){
-      if(this.hasNext()){
-        this.config.current++;
-        this.load();
-        return true;
+      let mainPromise=Promise.resolve(result);
+      for(let id=0;id<this.config.size;id++){
+        this.push(genPromise(id,mainPromise));
+        progress.run();
       }
-      return false;
-    }
 
-
-    previous(){
-      if(this.hasPrevious()){
-        this.config.current--;
-        this.load();
-        return true;
+      function genPromise(id:number,promise:Promise<any>){
+        
+        return new Promise(function(accept,reject){
+          promise
+            .then(function(data){
+              try{
+                accept(data[id]);
+              }catch(err){
+                reject(err);
+              }
+            }).catch(function(err){
+              reject(err);
+            })
+        });
       }
-      return false;
-    }
-
-    load(progress?:Progress|ProgressFunction){
-      this.length = 0;
-      if(!progress || typeof(progress)=='function')
-        progress = new Progress(this.config.progress)
-      progress=<Progress>progress;
-
-      progress.start(this.pageEnd-this.pageStart);    
-
+    }else{
       for(let idItem=this.pageStart;idItem<this.pageEnd;idItem++){
         let result:string |number | object | Promise<any>;
 
         try{
-          result = this.config.run(this.parameterList[idItem],this.genInfo());
+          if(this.parameterNumber){
+            result=this.config.run(this.parameterNumber,this.genInfo())
+          }else{
+            result = this.config.run(this.parameterList[idItem],this.genInfo());
+          }
+         
         }catch(e){
           result = e;
         }
@@ -182,27 +230,28 @@ export default class PagedArray extends Array implements PageInfo {
         progress.run();
       }
     }
-
-    forEach(callbackfn:any,thisArg?:any){
-      
-      function recursivePromise(arr:PagedArray){
-        //walk from page to page
-        Promise.all(arr)
-        .then(function(data){
-          data.forEach(callbackfn,thisArg)
-          if(arr.next()){
-            recursivePromise(arr);
-          }
-        });
-      }
-      this.config.current=0;
-      //run all parameters
-      let progress = new Progress(this.config.progress);
-
-      progress.start(this.parameters.length); 
-      this.load();
-
-      
-      recursivePromise(this);
-    };
   }
+
+  forEach(callbackfn:any,thisArg?:any){
+    
+    function recursivePromise(arr:PagedArray){
+      //walk from page to page
+      Promise.all(arr)
+      .then(function(data){
+        data.forEach(callbackfn,thisArg)
+        if(arr.next()){
+          recursivePromise(arr);
+        }
+      });
+    }
+    this.config.current=0;
+    //run all parameters
+    let progress = new Progress(this.config.progress);
+
+    progress.start(this.parameters.length); 
+    this.load();
+
+    
+    recursivePromise(this);
+  };
+}
