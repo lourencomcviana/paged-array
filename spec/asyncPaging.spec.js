f7deb5762353fb2709b43f50d87e3777a816c866
lourenço viana <lourenco.m.c.viana@gmail.com>
2017-09-13 16:13:38 -0300

    improved tests organization a lot

--- spec/asyncPaging.spec.js
+++ spec/asyncPaging.spec.js
@@ -5,7 +5,7 @@ const dummyRun =function(data,val){
   return new Promise(function(resolve,reject){
     setTimeout(function(){
       resolve(data);
-    },1000);
+    },0);
   })
   return data
 }
@@ -17,15 +17,78 @@ const dummyPageArray=function(){
 }
 
 
+var arr;
+
+beforeEach(function() {
+  arr=new PagedArray.default(testArr,{size:2,run:dummyRun});
+});
+
 describe("paging-create", function () {
-  it("should create a empty array ready to be loaded", function (done) {
-    //var product = calculator.multiply(2, 3);
- 
-    var arr=dummyPageArray();
+  it("should create a new instance of the array", function () {
+    expect(arr.constructor.name).toBe("PagedArray");
+  });
+
+  it("should load the first page of the array", function () {
+    expect(arr.length).toBe(0);
     arr.load();
-    arr[0].then(function(data){
-      expect(data).toBe(1);
+    expect(arr.length).toBe(2);
+  });
+});
+
+describe("paging-walking", function () {
+
+  beforeEach(function() {
+    arr.load();
+  });
+
+  it("should be in the first page", function () {
+    expect(arr.pageCurrent).toBe(0);
+  });
+
+  it("should read the second value", function (done) {
+    arr[1].then(function(data){
+      expect(data).toBe(2);
       done();
     });
   });
-});  
+
+  it("should be in the second page", function () {
+    arr.next();
+    expect(arr.pageCurrent).toBe(1);
+  });
+
+  it("should read the first value of the second page", function (done) {    
+    arr.next();
+    arr[0].then(function(data){
+      expect(data).toBe(3);
+      done();
+    });
+  });
+});
+
+describe("for-each", function () {
+  let promise;
+  let arr=dummyPageArray();
+
+  it("should return all itens of the array", function (done) {
+    let count=0;
+    promise=arr.forEach(function(element,id) {
+      count++;
+      expect(element).toBe(count);
+    }, this);
+
+    promise.then(function(end){
+      expect(count).toBe(3);
+      done();
+    });
+  });
+
+  it("the last page of the array should be [3]", function (done) {
+    promise.then(function(end){
+      expect(typeof end).toBe("object");
+      expect(end[0]).toBe(3);
+      done();
+    });
+  });
+  
+});
